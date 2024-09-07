08e27c5869089ba642f5cfac1063914c7bbfc707
Pablo N Manzur <g2ort@outlook.com>
2024-09-07 14:37:29 -0300

    Resolucion punto 1-2 y creacion txt vacio

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// Materia TP2
+// Curso Yatay- 2-1-A
+// Alumno: Pablo Manzur
+
 const mainArray = [2,10,"a",4,"b",6,"d",true,"e",9,1,"z",12,"r", "c", false]
+
+const condition = "number"
+
+//1
+function evaluar(arr, conditionValue) {
+    // 2
+  const newArr = arr.filter(item => typeof item === conditionValue)
+  console.log(newArr)
+}
+
+evaluar(mainArray, condition)
